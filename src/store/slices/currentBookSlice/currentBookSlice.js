5f2422b20c0725b3f57a4b0cbebc4266ac96f492
DrunkestManEvar <fullextentofsuperiority@mail.ru>
2021-05-17 11:23:20 +0300

    Fixed a bug where random book page would not fetch author name of the book

--- src/store/slices/currentBookSlice/currentBookSlice.js
+++ src/store/slices/currentBookSlice/currentBookSlice.js
@@ -13,42 +13,66 @@ const initialState = {
   error: false,
 };
 
+const fetchCurrentBookAuthor = async authorArray => {
+  if (!authorArray) return;
+  try {
+    const authorKey = authorArray[0].key;
+    const {
+      data: { name: authorName },
+    } = await axios.get(`https://openlibrary.org${authorKey}.json`);
+    return authorName;
+  } catch (error) {
+    console.error(error);
+    return 'Not Found';
+  }
+};
+
 export const fetchCurrentBook = createAsyncThunk(
   'currentBookSlice/fetchCurrentBook',
   async book => {
-    const { bookKey, author } = book;
+    try {
+      const { bookKey } = book;
+      let { author } = book;
 
-    const bookKeyValue = bookKey.slice(6);
+      const bookKeyValue = bookKey.slice(6);
 
-    if (bookKeyValue === 'undefined') {
-      return { author };
-    }
+      if (bookKeyValue === 'undefined') {
+        return { author };
+      }
 
-    if (bookKey) {
-      const bookResponse = await axios.get(
-        `https://infinite-plains-87110.herokuapp.com/https://openlibrary.org/${bookKey}.json`
-      );
+      if (bookKey) {
+        const bookResponse = await axios.get(
+          `https://infinite-plains-87110.herokuapp.com/https://openlibrary.org/${bookKey}.json`
+        );
 
-      const {
-        title,
-        publishers,
-        publish_date,
-        isbn_10,
-        isbn_13,
-        covers: coverId,
-      } = await bookResponse.data;
+        const {
+          title,
+          publishers,
+          publish_date,
+          isbn_10,
+          isbn_13,
+          covers: coverId,
+          authors,
+        } = await bookResponse.data;
 
-      const currentBook = {
-        title,
-        publishers,
-        publish_date,
-        isbn_10,
-        isbn_13,
-        author,
-        coverId,
-      };
+        if (!author) {
+          author = await fetchCurrentBookAuthor(authors);
+        }
 
-      return currentBook;
+        const currentBook = {
+          title,
+          publishers,
+          publish_date,
+          isbn_10,
+          isbn_13,
+          author,
+          coverId,
+        };
+
+        return currentBook;
+      }
+    } catch (error) {
+      console.error(error);
     }
   }
 );
